b71aca9c44ff85b3d9fcbb7a7b9355852aecd9ab
RiqueAlvess <henrique.alves.siqueira@hotmail.com>
2026-04-13 16:52:09 -0600

    Update generate-hash.ts

--- scripts/generate-hash.ts
+++ scripts/generate-hash.ts
@@ -1,7 +1,7 @@
 import { hashPassword } from '@/lib/auth';
 
 async function main() {
-  const password = process.argv[2] || 'pokemon.230H';
+  const password = process.argv[2] || 'alterarsenha123';
   const hash = await hashPassword(password);
 
   console.log(`\nPassword: ${password}`);
